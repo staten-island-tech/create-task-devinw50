feac4a8da40839e3792f7273e783ec601b06efa8
devinw50 <144460843+devinw50@users.noreply.github.com>
2024-01-29 18:48:14 -0500

    erg
    erg

--- dye.js
+++ dye.js
@@ -1,56 +1,26 @@
-const a = [ 1,2,3,4,5,6]
+let outcomes = [1, 2, 3, 4, 5, 6];
+let rolledNumbers = []; 
 
-Math.floor(Math.random() * 6) + 1;
+function rollDice() {
+  let guess = +document.getElementById('guess').value;
 
- 
-console.log(random);
+  if (rolledNumbers.length === outcomes.length) {
+    rolledNumbers = [];
+  }
 
- 
+  let remainingNumbers = outcomes.filter(num => !rolledNumbers.includes(num));
 
+  if (remainingNumbers.length > 0) {
+    let rolledNumberIndex = Math.floor(Math.random() * remainingNumbers.length);
+    let rolledNumber = remainingNumbers[rolledNumberIndex];
+    rolledNumbers.push(rolledNumber);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (guess === rolledNumber) {
+      document.getElementById('result').innerHTML = 'Congratulations!';
+    } else {
+      document.getElementById('result').innerHTML = 'Try again. The rolled number was ' + rolledNumber + '.';
+    }
+  } else {
+    document.getElementById('result').innerHTML = 'All numbers have been rolled. Please reset the game.';
+  }
+}
